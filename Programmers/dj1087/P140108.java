e1238991cfa1e93bc0a11e2de89e88c08f926a5c
Dongjun Jeong <sam1087@mju.ac.kr>
2023-05-14 00:16:26 +0900

    P140108: 문자열 나누기

--- Programmers/dj1087/P140108.java
+++ Programmers/dj1087/P140108.java
@@ -1,4 +1,35 @@
 package Programmers.dj1087;
 
-public class P42576 {
+public class P140108 {
+    public int solution(String s) {
+        int answer = 0;
+
+        char x = s.charAt(0);
+        int isX = 1;
+        int notX = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (isX == 0 && notX == 0) {
+                x = s.charAt(i);
+                isX++;
+                continue;
+            }
+            if (s.charAt(i) == x) {
+                isX++;
+            }else {
+                notX++;
+            }
+            if (isX == notX) {
+                answer++;
+                isX = 0;
+                notX = 0;
+            }
+            System.out.println(String.valueOf(x)+"|"+ String.valueOf(s.charAt(i))+"|"+
+                    String.valueOf(isX)+"|"+ String.valueOf(notX)+"|"+ String.valueOf(answer));
+        }
+        if (isX != notX) {
+            answer++;
+        }
+        return answer;
+
+    }
 }
