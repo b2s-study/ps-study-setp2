1ba6e8e46a04d0d96683da79142c12a1271c81d1
kingmingseo <multi1971@mju.ac.kr>
2023-05-13 15:20:41 +0900

    [P42576] 완주하지 못한 사람

--- Programmers/kingmingseo/P42576.java
+++ Programmers/kingmingseo/P42576.java
@@ -1,4 +1,24 @@
 package Programmers.kingmingseo;
 
-public class P42576 {
+import java.util.Arrays;
+
+class P42576 {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+       int i =0;
+        for(i=0; i<completion.length; i++)
+            if(!participant[i].equals(completion[i]))
+                break;
+
+
+        return participant[i];
+    }
+    public static void main(String[] args){
+        String[] part = {"leo", "kiki", "eden"};
+        String[] comp = {"eden", "kiki"};
+        P42576 sol = new P42576();
+        System.out.println(sol.solution(part, comp));
+    }
 }
