2a15d29ccb9b95343c2e253abd6905330df96a06
wonjunYou <youwjune@gmail.com>
2023-05-20 14:22:28 +0900

    P42576

--- Programmers/wonjunYou/P42576.java
+++ Programmers/wonjunYou/P42576.java
@@ -1,4 +1,38 @@
 package Programmers.wonjunYou;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P42576 {
+    public String solution(String[] participant, String[] completion) {
+
+        Map<String, Integer> counter = new HashMap<>();
+
+        for (String member : participant) {
+            if (counter.containsKey(member)) {
+                // map.get(key);
+                // map.put(key, value);
+                int count = counter.get(member);
+                counter.put(member, count + 1);
+            } else {
+                counter.put(member, 1);
+            }
+        }
+
+        return findMemberGiveUp(counter, completion);
+    }
+
+    private String findMemberGiveUp(Map<String, Integer> counter, String[] completion) {
+        for (String member : completion) {
+            int count = counter.get(member);
+            counter.put(member, count - 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : counter.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return "";
+    }
 }
